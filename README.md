fbb1348e6c0408fe19f6b984fd6649638cea6c75
zhuifengxia <liudanfeng@liudanfengdeMacBook-Pro.local>
2017-05-16 19:20:59 +0800

    ...

---
+++ README.md
@@ -0,0 +1,5 @@
+# RESTful Applications with Zend Framework [Example]
+
+This is an **example** to demonstrate the usage of [RESTful Zend Framework Extension](https://github.com/ahmadnassri/restful-zend-framework) which allows to create RESTful Controllers with ease.
+
+for implementation details please refer to [github.com/ahmadnassri/restful-zend-framework](https://github.com/ahmadnassri/restful-zend-framework).
---
+++ application/modules/api/controllers/FooController.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *  Sample Foo Resource
+ */
+class Api_FooController extends REST_Controller
+{
+    /**
+     * The index action handles index/list requests; it should respond with a
+     * list of the requested resources.
+     */
+    public function indexAction()
+    {
+        $this->view->message = 'indexAction has been called.';
+		echo "indexAction has been called.";exit;
+        $this->_response->ok();
+    }
+
+    /**
+     * The head action handles HEAD requests; it should respond with an
+     * identical response to the one that would correspond to a GET request,
+     * but without the response body.
+     */
+    public function headAction()
+    {
+        $this->view->message = 'headAction has been called';
+        $this->_response->ok();
+    }
+
+    /**
+     * The get action handles GET requests and receives an 'id' parameter; it
+     * should respond with the server resource state of the resource identified
+     * by the 'id' value.
+     */
+    public function getAction()
+    {
+        $id = $this->_getParam('id', 0);
+		echo $id;exit;
+        $this->view->id = $id;
+        $this->view->message = sprintf('Resource #%s', $id);
+        $this->_response->ok();
+    }
+
+    /**
+     * The post action handles POST requests; it should accept and digest a
+     * POSTed resource representation and persist the resource state.
+     */
+    public function postAction()
+    {
+        $this->view->params = $this->_request->getParams();
+        $this->view->message = 'Resource Created';
+        $this->_response->created();
+    }
+
+    /**
+     * The put action handles PUT requests and receives an 'id' parameter; it
+     * should update the server resource state of the resource identified by
+     * the 'id' value.
+     */
+    public function putAction()
+    {
+        $id = $this->_getParam('id', 0);
+
+        $this->view->id = $id;
+        $this->view->params = $this->_request->getParams();
+        $this->view->message = sprintf('Resource #%s Updated', $id);
+        $this->_response->ok();
+    }
+
+    /**
+     * The delete action handles DELETE requests and receives an 'id'
+     * parameter; it should update the server resource state of the resource
+     * identified by the 'id' value.
+     */
+    public function deleteAction()
+    {
+        $id = $this->_getParam('id', 0);
+
+        $this->view->id = $id;
+        $this->view->message = sprintf('Resource #%s Deleted', $id);
+        $this->_response->ok();
+    }
+}
